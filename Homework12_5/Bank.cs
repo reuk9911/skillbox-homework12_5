b1bc6f77d44dbfa3e73fc91c06be894992419933
виталий мальцев <reuk9911@yandex.ru>
2023-03-13 18:30:42 +0400

    first changes

---
+++ Homework12_5/Bank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework12_5
+{
+    public class Bank
+    {
+        public void Transfer(int BillId1, int BillId2) { }
+        public void OpenBill(int ClientId) { }
+        public void CloseBill(int ClientId) { }
+        public void DepositCash(int BillId, float Sum) { }
+        public void AddClient() { }
+    }
+}
